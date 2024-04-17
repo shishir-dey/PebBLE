c4ba00fc2b00d90cf3be8d741333456f191de5c8
Shishir Dey <shishirdey241097@gmail.com>
2024-04-17 22:23:27 +0530

    Initialize a React app

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
+};
